945cb20759809ecb9e3b74c558f48f9c0f29b9a8
saloni-vishwakarma <vsaloni443@gmail.com>
2025-03-18 12:24:11 +0530

    Add files via upload

---
+++ tast pragya/new cal.js
@@ -0,0 +1,84 @@
+var x, y, result
+var myinputbox = document.getElementById("inputbox");
+
+function input1() {
+
+    myinputbox.value+="1"
+}
+function input2() {
+   myinputbox.value+="2"
+}
+
+function input3() {
+   myinputbox.value+="3"
+}
+
+function input4() {
+  myinputbox
+  .value+="4"
+}
+
+function input5() {
+  myinputbox
+  .value+="5"
+}
+
+function input6() {
+  myinputbox
+  .value+="6"
+}
+
+function input7() {
+  myinputbox
+  .value+="7"
+}
+
+function input8() {
+  myinputbox
+  .value+="8"
+}
+
+function input9() {
+  myinputbox
+  .value+="9"
+}
+function inputplus() {
+  myinputbox
+  .value+="+"
+}
+function input0() {
+  myinputbox
+  .value+="0"
+} 
+function inputminus() {
+  myinputbox
+  .value+="-"
+} 
+function calculater(){
+  myinputbox
+  .value=eval(document.getElementById("inputbox").value)
+}
+function inputmulty() {
+  myinputbox
+  .value+="*"
+} 
+function inputdivide() {
+  myinputbox
+  .value+="/"
+} 
+function inputdot() {
+    myinputbox
+    .value+="."}
+
+    function inputpercent() {
+        myinputbox
+        .value+="%"}
+
+
+
+
+
+
+
+
+
